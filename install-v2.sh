0b8c03c16445cf6573d30146057f6d34cbb76347
Pongstr <twiz.tickler@gmail.com>
2013-12-19 02:09:42 +0800

    added wizard for v2.0.0-beta

---
+++ install-v2.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/sh
+
+echo ""
+echo "      ___       ___          ___          ___                   ___      "
+echo "     /  /\     /__/\        /  /\        /  /\         ___     /  /\     "
+echo "    /  /::\    \  \:\      /  /:/_      /  /:/_       /  /\   /  /::\    "
+echo "   /  /:/\:\    \  \:\    /  /:/ /\    /  /:/ /\     /  /:/  /  /:/\:\   "
+echo "  /  /:/~/:/_____\__\:\  /  /:/_/::\  /  /:/ /::\   /  /:/  /  /:/~/:/   "
+echo " /__/:/ /://__/::::::::\/__/:/__\/\:\/__/:/ /:/\:\ /  /::\ /__/:/ /:/___ "
+echo " \  \:\/:/ \  \:\~~\~~\/\  \:\ /~~/:/\  \:\/:/~/://__/:/\:\\  \:\/:::::/ "
+echo "  \  \::/   \  \:\  ~~~  \  \:\  /:/  \  \::/ /:/ \__\/  \:\\  \::/~~~~  "
+echo "   \  \:\    \  \:\       \  \:\/:/    \__\/ /:/       \  \:\\  \:\      "
+echo "    \  \:\    \  \:\       \  \::/       /__/:/         \__\/ \  \:\     "
+echo "     \__\/     \__\/        \__\/        \__\/                 \__\/     "
+echo ""
+echo "          Dotfiles (Pongstr v2.0.0) for setting up OSX Workspace.        "
+echo "                https://github.com/pongstr/dotfiles.git                  "
+
+# Download and install dotfiles
+curl -L -o dotfiles.zip https://github.com/pongstr/dotfiles/archive/v2.0.0-beta.zip && unzip dotfiles.zip
+mkdir -p "{$HOME}/Projects/"
+mv dotfiles-v2.0.0-beta "{$HOME}/Projects/dotfiles"
+ln -s "{$HOME}/Projects/dotfiles" "{$HOME}/dotfiles"
+
+cd "$HOME/Projects/dotfiles" 
+
+# Cleanup
+rm -rf "{$HOME}/dotfiles.zip"
