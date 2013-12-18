a99432917beea41a4823a5980cb2efb20deb6138
Pongstr <twiz.tickler@gmail.com>
2013-12-19 02:37:32 +0800

    okay this is it

--- install-v2.sh
+++ install-v2.sh
@@ -19,10 +19,10 @@ echo "          =======================================================        "
 echo ""
 # Download and install dotfiles
 curl -L -o dotfiles.zip https://github.com/pongstr/dotfiles/archive/v2.0.0-beta.zip && unzip dotfiles.zip
-mkdir -p "$HOME/Projects2/"
-mv dotfiles-v2.0.0-beta "$HOME/Projects/dotfiles"
-ln -s "$HOME/Projects2/dotfiles" "$HOME/dotfiles"
+mkdir -p $HOME/Projects2/
+mv dotfiles-v2.0.0-beta/* $HOME/Projects2/dotfiles
+ln -s $HOME/Projects2/dotfiles $HOME/dotfiles
 
-# Cleanup
-rm -rf "dotfiles-2.0.0-beta"
-rm -rf "install-v2.sh" && sh $HOME/Projects2/dotfiles/bootstrap.sh
+# Cleanup and run bootstrap.sh
+rm -rf dotfiles-2.0.0-beta dotfiles.zip
+rm -rf install-v2.sh && sh $HOME/Projects2/dotfiles/bootstrap.sh
