2219615abbbca5bd87d82f4c94491d5d6bb08cde
Pongstr <twiz.tickler@gmail.com>
2013-12-19 08:06:27 +0800

    cleanup for testing again

--- install-v2.sh
+++ install-v2.sh
@@ -15,14 +15,27 @@ echo "     \__\/     \__\/        \__\/        \__\/                 \__\/     "
 echo ""
 echo "          Dotfiles (Pongstr v2.0.0) for setting up OSX Workspace.        "
 echo "                https://github.com/pongstr/dotfiles.git                  "
-echo "          =======================================================        "
+echo "          .......................................................        "
 echo ""
+
+# Set Projects Directory here
+PROJECTSDIR="$HOME/Projects2"
+
+# Set Dotfiles version here
+DOTFILE="v2.0.0-beta"
+
 # Download and install dotfiles
-curl -L -o dotfiles.zip https://github.com/pongstr/dotfiles/archive/v2.0.0-beta.zip && unzip dotfiles.zip
-mkdir -p $HOME/Projects2/
-mv dotfiles-v2.0.0-beta/* $HOME/Projects2/dotfiles
-ln -s $HOME/Projects2/dotfiles $HOME/dotfiles
+curl -L -o dotfiles.zip "https://github.com/pongstr/dotfiles/archive/$DOTFILE.zip" && unzip dotfiles.zip
+
+# Create Projects Directory
+mkdir -p $PROJECTSDIR
+
+# Move unzipped files to $PROJECTSDIR
+mv dotfiles-v2.0.0-beta $PROJECTSDIR/dotfiles
+
+# Create dotfiles alias to Home folder for easier access
+ln -s $PROJECTSDIR/dotfiles $HOME/dotfiles
 
 # Cleanup and run bootstrap.sh
 rm -rf dotfiles-2.0.0-beta dotfiles.zip
-rm -rf install-v2.sh && sh $HOME/dotfiles/bootstrap.sh
+rm -rf install-v2.sh && sh $PROJECTSDIR/dotfiles/bootstrap.sh
